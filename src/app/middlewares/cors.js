d663705845a971775b8f4087d21092a373fb8a84
Kauan Portela <kauanphbbb@gmail.com>
2024-03-17 18:02:56 -0300

    feat: add permission to many origins cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,7 +1,15 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Allow-Max-Age', '10');
+  const allowedOrigins = ['http://localhost:3000', 'http://localhost:3002'];
+
+  const origin = request.header('origin');
+  const isAllowedOrigin = allowedOrigins.includes(origin);
+
+  if (isAllowedOrigin) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Allow-Max-Age', '10');
+  }
+
   next();
 };
